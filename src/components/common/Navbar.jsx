d6f53b68bf46135447f496ad63430d76118db999
harshsindhu0408 <harshsindhu0402@gmail.com>
2023-09-23 15:40:43 +0530

    home-page

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -16,22 +16,22 @@ const Navbar = () => {
       const [isLoggedin, setIsLoggedin] = useState(true);
 
   return (
-    <nav className='w-full h-20 bg-transparent flex items-center justify-center shadow-lg'>
-        <div className='w-11/12 flex flex-row items-center justify-between'>
+    <nav className='w-full bg-slate-800 absolute z-10 h-20 bg-transparent flex items-center justify-center shadow-lg'>
+        <div className='w-9/12 mr-20 flex flex-row items-center justify-between'>
             {/* left part with name and logo */}
             <div className='flex flex-row items-center justify-center gap-2'>
             <Link className='flex flex-row items-center justify-center gap-2' to={"/"}>
               <img  src={logo} alt="loggoo" width="45px"></img>
-              <p className='text-2xl font-bold'>RescueConnect</p>
+              <p className='text-2xl text-white font-Roberto font-bold'>RescueConnect</p>
             </Link>
             </div>
 
             {/* mid part with links and all */}
             <div>
-                <ul className='flex items-center justify-center gap-8 mr-24'>
+                <ul className='flex items-center mr-10 justify-center gap-8'>
                     {Links.map((link) => (
                         <Link to={link.link}
-                        className="text-ternary-dark font-Roborto text-lg hover:text-indigo-500 duration-200">
+                        className="text-white font-bold font-Roborto hover:text-indigo-500 duration-200">
                             {link.name}
                         </Link>
                     ))}
@@ -42,8 +42,8 @@ const Navbar = () => {
             <div className='flex items-center justify-center gap-4'>
                 {
                     isLoggedin ?
-                    (<button className=' bg-indigo-500 hover:bg-indigo-600 block text-white shadow-sm rounded-sm px-4 py-2 duration-300 w-20' onClick={() => setIsLoggedin(false)}>Login</button>) :
-                    (<button className=' bg-indigo-500 hover:bg-indigo-600 block text-white shadow-sm rounded-sm px-4 py-2 duration-300 w-20' onClick={() => setIsLoggedin(true)}>Logout</button>)
+                    (<button className=' bg-indigo-500 hover:bg-indigo-600 block font-bold text-white shadow-sm rounded-full px-4 py-2 duration-300 w-20' onClick={() => setIsLoggedin(false)}>Login</button>) :
+                    (<button className=' bg-indigo-500 hover:bg-indigo-600 block font-bold text-white shadow-sm rounded-full px-4 py-2 duration-300 w-20' onClick={() => setIsLoggedin(true)}>Logout</button>)
                 }
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className=''>
-      This is the Homepage 🔴
+    <div className='w-full absolute mainSection bg-no-repeat h-full overflow-x-hidden flex items-start justify-center'>
+      
+      {/* Main hero section (landing page) */}
+      <div className='w-8/12 text-center flex flex-col items-center mt-48 justify-center gap-y-8'>
+        <p className='w-8/12 text-white font-Roborto overflow-hidden font-extrabold h-32 text-6xl'> Connect and Coordinate During Calamities </p>
+        <p className=' w-6/12 font-Roborto text-white overflow-hidden text-2xl'>RescueConnect is a Web Application that help rescue agencies 
+          coordinate and display their locations during natural or man-made calamaties</p>
+      </div>
+
     </div>
   )
 }
 
 export default Home
+
+// Connect and Coordinate During Calamities
+// RescueConnect is a Web Application that help rescue agencies coordinate and display their locations during natural or man-made calamaties
