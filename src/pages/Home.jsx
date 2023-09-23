5b68130a8f8c0cd4a0062285225157ba2fe0e1b7
harshsindhu0408 <harshsindhu0402@gmail.com>
2023-09-23 15:48:18 +0530

    added banners to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import Banner from '../components/home/Banners'
 
 const Home = () => {
   return (
-    <div className='w-full absolute mainSection bg-no-repeat h-full overflow-x-hidden flex items-start justify-center'>
+    <div className='w-full absolute mainSection bg-no-repeat overflow-x-hidden flex flex-col items-center justify-center'>
       
       {/* Main hero section (landing page) */}
-      <div className='w-8/12 text-center flex flex-col items-center mt-48 justify-center gap-y-8'>
+      <div className='w-8/12 text-center flex flex-col items-center mt-52 justify-center gap-y-8'>
         <p className='w-8/12 text-white font-Roborto overflow-hidden font-extrabold h-32 text-6xl'> Connect and Coordinate During Calamities </p>
         <p className=' w-6/12 font-Roborto text-white overflow-hidden text-2xl'>RescueConnect is a Web Application that help rescue agencies 
           coordinate and display their locations during natural or man-made calamaties</p>
       </div>
 
+      <div className='mt-20'>
+        <Banner />
+      </div>
+
     </div>
+
   )
 }
 
