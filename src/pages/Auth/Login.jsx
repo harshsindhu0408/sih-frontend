7c95db9e0fc29a6c15abf099ab0c8528c5ddec26
harshsindhu0408 <harshsindhu0402@gmail.com>
2023-09-24 12:54:08 +0530

    added signup page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,225 +1,304 @@
 import React, { useState } from "react";
-import "./Login.css";
 import { Link } from "react-router-dom";
+import bgimg from "../../assets/signup.png";
 
 const Login = () => {
-  const style = {
-    overflow: "hidden",
-    marginTop: "10px",
-    paddingBottom: "5px",
-  };
-  const [showLogin, setShowLogin] = useState(true); // State to track login/registration section
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [expertise, setExpertise] = useState("");
 
-  const [signInDetails, setSignInDetails] = useState({
-    SignInemail: "",
-    SignInPassword: "",
-  });
-  const [signUpDetails, setsignUpDetails] = useState({
-    AgencyName: "",
-    email: "",
-    password: "",
-    PhoneNumber: "",
-    Experties: "",
-  });
+  const statesOfIndia = [
+    "Andaman and Nicobar Islands",
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chandigarh",
+    "Chhattisgarh",
+    "Dadra and Nagar Haveli and Daman and Diu",
+    "Delhi",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Lakshadweep",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Puducherry",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal",
+  ];
 
-  const handleSignInChanges = (e) => {
-    const { name, value } = e.target;
-    setSignInDetails((user) => ({ ...user, [name]: value }));
-    console.log(signInDetails);
-  };
-  const handleSignUpChanges = (e) => {
-    const { name, value } = e.target;
-    setsignUpDetails((user) => ({ ...user, [name]: value }));
-    console.log(signUpDetails);
-  };
-
-  const handleToggleSection = () => {
-    setShowLogin(!showLogin); // Toggle between login and registration
-  };
-
-  const [signInRecords, setSignInRecords] = useState([]);
-  const [signUpRecords, setSignUpRecords] = useState([]);
-  const handleSignInSubmit = (e) => {
+  // Handle form submission
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    const newSignInRecords = { ...signInDetails };
-    setSignInRecords([...signInRecords, newSignInRecords]);
-    console.log(signInDetails);
-  };
-
-  const handleSignUpSubmit = (e) => {
-    e.preventDefault();
-    const newSignUpRecords = { ...signUpDetails };
-    setSignUpRecords([...signUpRecords, newSignUpRecords]);
-    console.log(signUpDetails);
+    // You can add your signup logic here
+    // Typically, you would make an API request to your server to create a new agency
   };
 
   return (
-    <>
-      <div>
-        <div className="background" />
-        <div className="container">
-          <div className="item">
-            <h2 className="logo">
-              <i className="bx bxl-xing" />
-              Smart India Hackathon
-            </h2>
-            <div className="text-item">
-              <h2>
-                Welcome! <br />
-                <span>We Rescue People in Need</span>
-              </h2>
+    <div className="flex flex-col items-center justify-center bg-gray-50 min-h-screen">
+      <div className="w-11/12 flex flex-col items-center justify-center lg:flex-row xl:flex-row 2xl:flex-row sm:flex-col flex-wrap">
+
+        {/* Left Section (Signup Form) */}
+        <div className="lg:w-7/12  sm:w-full p-8">
+          <h2 className="text-3xl overflow-hidden font-extrabold text-gray-900">
+            Signup for your Agency
+          </h2>
+
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+            {/* Name */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="name" className="sr-only">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
             </div>
-          </div>
-          <div className={`login-section ${showLogin ? "active" : ""}`}>
-            <div className="form-box login">
-              <form onSubmit={handleSignInSubmit}>
-                <h2>{showLogin ? "Sign Up" : "Sign In"}</h2>
-                <div className="input-box">
-                  <span className="icon">
-                    <i className="bx bxs-envelope" />
-                  </span>
-                  <input
-                    type="email"
-                    autoComplete="off"
-                    name="SignInemail"
-                    id="SignInemail"
-                    value={signInDetails.email}
-                    onChange={handleSignInChanges}
-                    required={true}
-                  />
-                  <label htmlFor="">Email</label>
-                </div>
-                <div className="input-box">
-                  <span className="icon">
-                    <i className="bx bxs-lock-alt" />
-                  </span>
-                  <input
-                    type="password"
-                    autoComplete="off"
-                    name="SignInPassword"
-                    id="SignInPassword"
-                    value={signInDetails.password}
-                    onChange={handleSignInChanges}
-                    required={true}
-                  />
-                  <label htmlFor="">Password</label>
-                </div>
-                <div className="remember-password">
-                  <label>
-                    <input type="checkbox" />
-                    Remember Me
-                  </label>
-                  <Link to="/">Forget Password</Link>
-                </div>
-                <button className="btn">Login In</button>
-                <div className="create-account">
-                  <p>
-                    {showLogin
-                      ? "Create A New Account? "
-                      : "Already Have An Account? "}
-                    <button
-                      className="toggle-button"
-                      onClick={handleToggleSection}
-                    >
-                      {showLogin ? "Sign Up" : "Sign In"}
-                    </button>
-                  </p>
-                </div>
-              </form>
+
+            {/* Email */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
             </div>
-            <div className="flex flex-col form-box register">
-              <h2 style={style}>Sign Up</h2>
-              <form className="signUp" onSubmit={handleSignUpSubmit}>
-                <div className="input-box">
-                  <input
-                    type="text"
-                    name="AgencyName"
-                    id="AgencyName"
-                    value={signUpDetails.AgencyName}
-                    onChange={handleSignUpChanges}
-                    autoComplete="off"
-                    required={true}
-                  />
-                  <label>Agency Name</label>
-                </div>
-                <div className="input-box">
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={signUpDetails.email}
-                    onChange={handleSignUpChanges}
-                    autoComplete="off"
-                    required={true}
-                  />
-                  <label>Email</label>
-                </div>
-                <div className="input-box">
-                  <span className="icon">
-                    <i className="bx bxs-lock-alt" />
-                  </span>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={signUpDetails.password}
-                    onChange={handleSignUpChanges}
-                    autoComplete="off"
-                    required={true}
-                  />
-                  <label>Password</label>
-                </div>
-                <div className="input-box">
-                  <input
-                    type="number"
-                    name="PhoneNumber"
-                    id="PhoneNumber"
-                    value={signUpDetails.PhoneNumber}
-                    onChange={handleSignUpChanges}
-                    autoComplete="off"
-                    required={true}
-                  />
-                  <label>Phone Number</label>
-                </div>
-                <div className="input-box">
-                  <input
-                    type="text"
-                    name="Experties"
-                    id="Experties"
-                    value={signUpDetails.Experties}
-                    onChange={handleSignUpChanges}
-                    autoComplete="off"
-                    required={true}
-                  />
-                  <label>Experties</label>
-                </div>
-                <div className="remember-password">
-                  <label htmlFor="">
-                    <input type="checkbox" required={true} />I agree with this
-                    statement
-                  </label>
-                </div>
-                <button className="btn">Register</button>
-                <div className="create-account">
-                  <p>
-                    {showLogin
-                      ? "Create A New Account? "
-                      : "Already Have An Account? "}
-                    <button
-                      className="toggle-button"
-                      onClick={handleToggleSection}
-                    >
-                      {showLogin ? "Sign In" : "Sign Up"}
-                    </button>
-                  </p>
-                </div>
-              </form>
+
+            {/* Password */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
             </div>
-          </div>
+
+            {/* Street */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="street" className="sr-only">
+                  Street
+                </label>
+                <input
+                  id="street"
+                  name="street"
+                  type="text"
+                  autoComplete="street"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Street"
+                  value={street}
+                  onChange={(e) => setStreet(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* City */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="city" className="sr-only">
+                  City
+                </label>
+                <input
+                  id="city"
+                  name="city"
+                  type="text"
+                  autoComplete="city"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="City"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* State */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="state" className="sr-only">
+                  State
+                </label>
+                <select
+                  id="state"
+                  name="state"
+                  autoComplete="state"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  onChange={(e) => setState(e.target.value)}
+                  value={state}
+                >
+                  <option value="">Select State</option>
+                  {statesOfIndia.map((stateName) => (
+                    <option key={stateName} value={stateName}>
+                      {stateName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            {/* Postal Code */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="postalCode" className="sr-only">
+                  Postal Code
+                </label>
+                <input
+                  id="postalCode"
+                  name="postalCode"
+                  type="text"
+                  autoComplete="postalCode"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Postal Code"
+                  value={postalCode}
+                  onChange={(e) => setPostalCode(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Country */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="country" className="sr-only">
+                  Country
+                </label>
+                <input
+                  id="country"
+                  name="country"
+                  type="text"
+                  autoComplete="country"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Country"
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Phone Number */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="phoneNumber" className="sr-only">
+                  Phone Number
+                </label>
+                <input
+                  id="phoneNumber"
+                  name="phoneNumber"
+                  type="tel"
+                  autoComplete="tel"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Phone Number"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Expertise */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="expertise" className="sr-only">
+                  Expertise
+                </label>
+                <input
+                  id="expertise"
+                  name="expertise"
+                  type="text"
+                  autoComplete="expertise"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Expertise"
+                  value={expertise}
+                  onChange={(e) => setExpertise(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
         </div>
+
+        {/* Right Section (Image) */}
+        <div className="lg:w-5/12  sm:w-full flex items-center justify-center">
+          <img
+            src={bgimg} // Replace with the actual image path
+            alt="Agency"
+            width="800px"
+            className="object-cover ml-10 object-center sm:h-auto"
+          />
+        </div>
+
       </div>
-    </>
+    </div>
   );
 };
 
