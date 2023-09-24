12f7fe15e6826c622e4c58cec88ecf9693496c39
AdityaRaj Gautam <404arg@gmail.com>
2023-09-24 11:18:33 +0530

    added footer and login

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='text-black font-Roborto flex-items-centerjustify-center text-center text-4xl text-extrabold'>
-      This is the footer
-    </div>
-  )
-}
+    <footer className="w-full bg-gray-800 text-white p-10 shadow-lg ">
+      <div
+        className="underline"
+        style={{ fontSize: "70px", marginBottom: "50px" }}
+      >
+        Contact Us
+      </div>
+      <div className=" mb-10 flex flex-row items-center justify-center">
+        <div className="basis-2/4 mb-10">
+          <div>
+            <img src={logo} alt="" width="70px" />
+          </div>
+          <div style={{ overflow: "hidden" }} className="mt-6 text-2xl">
+            <span>We Rescue the people in Need</span>
+          </div>
+          <div></div>
+        </div>
+        <div className="basis-1/4 ">
+          <ul className="space-y-2">
+            <li>
+              <p className="md:font-bold underline">Company</p>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/about">Our Team</Link>
+            </li>
+            <li>
+              <Link to="/">Careers</Link>
+            </li>
+            <li>
+              <Link to="/">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="basis-1/4">
+          <ul className="space-y-2">
+            <li>
+              <p className="font-bold underline">Legal</p>
+            </li>
+            <li>
+              <Link to="/">Blog</Link>
+            </li>
+            <li>
+              <Link to="/">Case Studies</Link>
+            </li>
+            <li>
+              <Link to="/">FAQs</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="mt-5">© 2020 Your Company, Inc. All rights reserved.</div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,31 +1,226 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const style = {
+    overflow: "hidden",
+    marginTop: "10px",
+    paddingBottom: "5px",
+  };
+  const [showLogin, setShowLogin] = useState(true); // State to track login/registration section
+
+  const [signInDetails, setSignInDetails] = useState({
+    SignInemail: "",
+    SignInPassword: "",
+  });
+  const [signUpDetails, setsignUpDetails] = useState({
+    AgencyName: "",
+    email: "",
+    password: "",
+    PhoneNumber: "",
+    Experties: "",
+  });
+
+  const handleSignInChanges = (e) => {
+    const { name, value } = e.target;
+    setSignInDetails((user) => ({ ...user, [name]: value }));
+    console.log(signInDetails);
+  };
+  const handleSignUpChanges = (e) => {
+    const { name, value } = e.target;
+    setsignUpDetails((user) => ({ ...user, [name]: value }));
+    console.log(signUpDetails);
+  };
+
+  const handleToggleSection = () => {
+    setShowLogin(!showLogin); // Toggle between login and registration
+  };
+
+  const [signInRecords, setSignInRecords] = useState([]);
+  const [signUpRecords, setSignUpRecords] = useState([]);
+  const handleSignInSubmit = (e) => {
+    e.preventDefault();
+
+    const newSignInRecords = { ...signInDetails };
+    setSignInRecords([...signInRecords, newSignInRecords]);
+    console.log(signInDetails);
+  };
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    const newSignUpRecords = { ...signUpDetails };
+    setSignUpRecords([...signUpRecords, newSignUpRecords]);
+    console.log(signUpDetails);
+  };
+
   return (
     <>
-      <div className="box">
-        <form>
-          <h2>Sign In</h2>
-          <div className="inputBox">
-            {/* <AiOutlineMail/> */}
-            <input type="email " required="true"></input>
-            <label for="">Email</label>
-            <i></i>
+      <div>
+        <div className="background" />
+        <div className="container">
+          <div className="item">
+            <h2 className="logo">
+              <i className="bx bxl-xing" />
+              Smart India Hackathon
+            </h2>
+            <div className="text-item">
+              <h2>
+                Welcome! <br />
+                <span>We Rescue People in Need</span>
+              </h2>
+            </div>
           </div>
-          <div className="inputBox">
-            <input type="password" required="true"></input>
-            <label for="">Password</label>
-            <i></i>
+          <div className={`login-section ${showLogin ? "active" : ""}`}>
+            <div className="form-box login">
+              <form onSubmit={handleSignInSubmit}>
+                <h2>{showLogin ? "Sign Up" : "Sign In"}</h2>
+                <div className="input-box">
+                  <span className="icon">
+                    <i className="bx bxs-envelope" />
+                  </span>
+                  <input
+                    type="email"
+                    autoComplete="off"
+                    name="SignInemail"
+                    id="SignInemail"
+                    value={signInDetails.email}
+                    onChange={handleSignInChanges}
+                    required={true}
+                  />
+                  <label htmlFor="">Email</label>
+                </div>
+                <div className="input-box">
+                  <span className="icon">
+                    <i className="bx bxs-lock-alt" />
+                  </span>
+                  <input
+                    type="password"
+                    autoComplete="off"
+                    name="SignInPassword"
+                    id="SignInPassword"
+                    value={signInDetails.password}
+                    onChange={handleSignInChanges}
+                    required={true}
+                  />
+                  <label htmlFor="">Password</label>
+                </div>
+                <div className="remember-password">
+                  <label>
+                    <input type="checkbox" />
+                    Remember Me
+                  </label>
+                  <Link to="/">Forget Password</Link>
+                </div>
+                <button className="btn">Login In</button>
+                <div className="create-account">
+                  <p>
+                    {showLogin
+                      ? "Create A New Account? "
+                      : "Already Have An Account? "}
+                    <button
+                      className="toggle-button"
+                      onClick={handleToggleSection}
+                    >
+                      {showLogin ? "Sign Up" : "Sign In"}
+                    </button>
+                  </p>
+                </div>
+              </form>
+            </div>
+            <div className="flex flex-col form-box register">
+              <h2 style={style}>Sign Up</h2>
+              <form className="signUp" onSubmit={handleSignUpSubmit}>
+                <div className="input-box">
+                  <input
+                    type="text"
+                    name="AgencyName"
+                    id="AgencyName"
+                    value={signUpDetails.AgencyName}
+                    onChange={handleSignUpChanges}
+                    autoComplete="off"
+                    required={true}
+                  />
+                  <label>Agency Name</label>
+                </div>
+                <div className="input-box">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={signUpDetails.email}
+                    onChange={handleSignUpChanges}
+                    autoComplete="off"
+                    required={true}
+                  />
+                  <label>Email</label>
+                </div>
+                <div className="input-box">
+                  <span className="icon">
+                    <i className="bx bxs-lock-alt" />
+                  </span>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={signUpDetails.password}
+                    onChange={handleSignUpChanges}
+                    autoComplete="off"
+                    required={true}
+                  />
+                  <label>Password</label>
+                </div>
+                <div className="input-box">
+                  <input
+                    type="number"
+                    name="PhoneNumber"
+                    id="PhoneNumber"
+                    value={signUpDetails.PhoneNumber}
+                    onChange={handleSignUpChanges}
+                    autoComplete="off"
+                    required={true}
+                  />
+                  <label>Phone Number</label>
+                </div>
+                <div className="input-box">
+                  <input
+                    type="text"
+                    name="Experties"
+                    id="Experties"
+                    value={signUpDetails.Experties}
+                    onChange={handleSignUpChanges}
+                    autoComplete="off"
+                    required={true}
+                  />
+                  <label>Experties</label>
+                </div>
+                <div className="remember-password">
+                  <label htmlFor="">
+                    <input type="checkbox" required={true} />I agree with this
+                    statement
+                  </label>
+                </div>
+                <button className="btn">Register</button>
+                <div className="create-account">
+                  <p>
+                    {showLogin
+                      ? "Create A New Account? "
+                      : "Already Have An Account? "}
+                    <button
+                      className="toggle-button"
+                      onClick={handleToggleSection}
+                    >
+                      {showLogin ? "Sign In" : "Sign Up"}
+                    </button>
+                  </p>
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="links">
-            <a href="#">Forgot Password</a>
-            <a href="#">Sign Up</a>
-          </div>
-          <input type="submit" value="Login"/>
-        </form>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
