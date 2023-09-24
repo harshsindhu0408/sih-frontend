6a7738649ae416174402905ce3b412fdacfee3f4
harshsindhu0408 <harshsindhu0402@gmail.com>
2023-09-24 14:03:53 +0530

    added login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import bgimg from "../../assets/vecteezy_3d-male-character-happy-working-on-a-laptop_24387907_314.png";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-50 min-h-screen">
+      <div className="w-11/12 flex flex-col items-center justify-center lg:flex-row xl:flex-row 2xl:flex-row sm:flex-col flex-wrap">
+        {/* Left Section (Signup Form) */}
+        <div className="lg:w-7/12  sm:w-full p-8">
+          <h2 className="text-4xl overflow-hidden font-extrabold text-gray-900">
+            Login to Your Account
+          </h2>
+
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+
+            {/* Email */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Password */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Dont have an accont */}
+            <div className="mt-4 text-center">
+              <p className="text-sm text-gray-600">
+                Don't have an account?{" "}
+                <Link
+                  to="/signup"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+
+            {/* Submit Button */}
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Login
+              </button>
+            </div>
+
+          </form>
+        </div>
+
+        {/* Right Section (Image) */}
+        <div className="lg:w-5/12  sm:w-full flex items-center justify-center">
+          <img
+            src={bgimg} // Replace with the actual image path
+            alt="Agency"
+            width="1000px"
+            className="object-cover ml-10 object-center sm:h-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
