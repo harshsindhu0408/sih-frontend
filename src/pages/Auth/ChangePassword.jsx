1862a021f590c5d7a593908c9f853b329bfe27ea
harshsindhu0408 <harshsindhu0402@gmail.com>
2023-09-25 12:16:22 +0530

    responsiveness to contact us page

--- src/pages/Auth/ChangePassword.jsx
+++ src/pages/Auth/ChangePassword.jsx
@@ -6,85 +6,80 @@ const ChangePassword = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
 
-  // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Check if the new password and confirm new password match
     if (newPassword !== confirmNewPassword) {
       alert("New password and confirm new password do not match.");
       return;
     }
 
-    // Add logic here to send the password update request to your backend
-
-    // Reset form fields
     setOldPassword("");
     setNewPassword("");
     setConfirmNewPassword("");
   };
 
   return (
-    <div className="mx-auto flex flex-col items-center justify-center w-screen h-screen">
-      <div className="w-full flex flex-row flex-wrap items-center justify-center gap-x-5">
-        {/* left div with password changes */}
-        <div className="2xl:w-6/12 sm:w-[200px] h-full gap-y-12 flex flex-col items-center justify-center">
-          <p className="text-4xl text-center font-roborto 2xl:h-12 overflow-hidden">Change Your Password</p>
-          <form onSubmit={handleSubmit} className="2xl:w-[400px] sm:w-[200px]">
-            <div className="mb-4">
-              <label htmlFor="oldPassword" className="block mb-2">
-                Old Password
-              </label>
-              <input
-                type="password"
-                id="oldPassword"
-                value={oldPassword}
-                onChange={(e) => setOldPassword(e.target.value)}
-                className="w-full px-3 py-2 border rounded-md"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="newPassword" className="block mb-2">
-                New Password
-              </label>
-              <input
-                type="password"
-                id="newPassword"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                className="w-full px-3 py-2 border rounded-md"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="confirmNewPassword" className="block mb-2">
-                Confirm New Password
-              </label>
-              <input
-                type="password"
-                id="confirmNewPassword"
-                value={confirmNewPassword}
-                onChange={(e) => setConfirmNewPassword(e.target.value)}
-                className="w-full px-3 py-2 border rounded-md"
-                required
-              />
-            </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                className="bg-indigo-500 mt-4 text-white transition-all duration-200 px-4 py-2 rounded-full hover:bg-indigo-600"
-              >
-                Change Password
-              </button>
-            </div>
-          </form>
-        </div>
+    <div className="mx-auto w-9/12 flex flex-col md:flex-row items-center justify-center min-h-screen">
+      {/* Left div with password changes */}
+      <div className="w-full md:w-1/2 lg:w-2/3 xl:w-2/3 2xl:w-2/3 gap-y-6 md:gap-y-0 flex flex-col items-center justify-center md:items-start">
+        <p className="text-4xl text-center sm:h-auto font-Roborto mb-4 font-bold overflow-hidden md:text-left font-roboto">
+          Change Your Password
+        </p>
+        <form onSubmit={handleSubmit} className="w-full md:w-[400px]">
+          <div className="mb-4">
+            <label htmlFor="oldPassword" className="block mb-2">
+              Old Password
+            </label>
+            <input
+              type="password"
+              id="oldPassword"
+              value={oldPassword}
+              onChange={(e) => setOldPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="newPassword" className="block mb-2">
+              New Password
+            </label>
+            <input
+              type="password"
+              id="newPassword"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="confirmNewPassword" className="block mb-2">
+              Confirm New Password
+            </label>
+            <input
+              type="password"
+              id="confirmNewPassword"
+              value={confirmNewPassword}
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            />
+          </div>
+          <div className="text-center md:text-left">
+            <button
+              type="submit"
+              className="bg-indigo-500 mt-4 sm:w-full text-white transition-all duration-200 px-4 py-2 rounded-full hover:bg-indigo-600"
+            >
+              Change Password
+            </button>
+          </div>
+        </form>
+      </div>
 
-        {/* right div with png */}
-        <div className="">
-          <img src={bgimg} alt="sfg" width="800px" />
-        </div>
+      {/* Right div with image */}
+      <div className="hidden md:block w-full md:w-1/2 lg:w-2/3 xl:w-2/3 2xl:w-2/3">
+        <img src={bgimg} alt="sfg" width="800px" />
       </div>
     </div>
   );
