695583bc0fc47e1d757d3d0e9ae4c594daa8301a
x1mukesh <of.mukesh@gmail.com>
2023-09-27 00:29:36 +0530

    check

--- src/components/DisasterDetails.jsx
+++ src/components/DisasterDetails.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import apiConnector from "../services/apiConnector";
 import { disasterEndPoints } from "../services/api";
 import { toast } from "react-toastify";
-import { Link, useNavigate, useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DisasterDetails = () => {
   // Extract the disasterId from the URL
   const { disasterId } = useParams();
 
-  const [disasters, setDisasters] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [disaster, setDisaster] = useState(null);
+  const [loadingData, setLoadingData] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,28 +17,72 @@ const DisasterDetails = () => {
       try {
         const response = await apiConnector({
           method: "GET",
-          url: disasterEndPoints.GET_DISASTER_API + '/' + disasterId,
+          url: `${disasterEndPoints.GET_DISASTER_API}/${disasterId}`,
         });
 
-        setDisasters(response.disaster);
-        setLoading(false);
+        setDisaster(response.disaster); // Assuming the API response contains the disaster details
+        setLoadingData(false);
       } catch (error) {
-        toast.error("Error fetching disasters");
+        toast.error("Error fetching disaster details");
         console.error(error);
-        setLoading(false);
+        setLoadingData(false);
       }
     };
 
     fetchData();
-  }, []);
-  console.log(disasters);
+  }, [disasterId]);
 
   return (
-    <div>
-      {/* Display the details of the disaster */}
-      <h2>Disaster Details</h2>
-      <p>Disaster ID: {disasterId}</p>
-      {/* Display other details of the disaster */}
+    <div className="mt-4">
+      {loadingData ? (
+        <div className="w-full flex items-center justify-center">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-purple-500"></div>
+        </div>
+      ) : disaster ? (
+        <div>
+          {/* Display the details of the disaster */}
+          <h2 className="text-3xl font-semibold mb-4 text-purple-700">
+            Disaster Details
+          </h2>
+          <div className="bg-white shadow-md p-4 rounded-lg">
+            <h2>Disaster Details</h2>
+            <p className="text-sm text-gray-500 mt-2">
+              Disaster ID: {disasterId}
+            </p>
+            <p className="text-sm text-gray-500 mt-2">
+              Severity: {disaster.severity}
+            </p>
+            
+            {/* Add more fields below */}
+            <p className="text-sm text-gray-500 mt-2">
+              Type of Disaster: {disaster.typeOfDisaster}
+            </p>
+            <p className="text-sm text-gray-500 mt-2">
+              Description: {disaster.description}
+            </p>
+            <p className="text-sm text-gray-500 mt-2">
+              Status: {disaster.status}
+            </p>
+            {/* Add more fields as needed */}
+          </div>
+          <button
+            onClick={() => navigate("/disasters")} // Redirect back to the disasters list
+            className="mt-4 bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded-full"
+          >
+            Back to Disasters
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p className="text-red-500">Disaster not found.</p>
+          <button
+            onClick={() => navigate("/disasters")} // Redirect back to the disasters list
+            className="mt-4 bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded-full"
+          >
+            Back to Disasters
+          </button>
+        </div>
+      )}
     </div>
   );
 };
