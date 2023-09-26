7a49eea14cf5e14778472dc94c5acb05c8ac016b
x1mukesh <of.mukesh@gmail.com>
2023-09-26 10:52:19 +0530

    agency reducer update

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,13 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { combineReducers } from "redux";
 import authReducer from "./Reducers/authRedcuer";
 import profileReducer from "./Reducers/profileReducer";
+import agencyReducer from "./Reducers/agencyReducer";
 
 const store = createStore(
   combineReducers({
     auth: authReducer,
     profile: profileReducer,
+    agencies: agencyReducer,
   }),
   composeWithDevTools(applyMiddleware(thunk))
 );
