804964b45a5eecec885943ea9dc148791bea5aa6
harshsindhu0408 <harshsindhu0402@gmail.com>
2023-09-27 22:45:36 +0530

    updated resource

--- src/components/AddResource.jsx
+++ src/components/AddResource.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
-import apiConnector from "../services/apiConnector";
-import { resourceEndPoints } from "../services/api"; // Import your API endpoints
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -11,7 +9,8 @@ const AddResources = () => {
   const [quantity, setQuantity] = useState("");
   const [status, setStatus] = useState("");
   const [availability, setAvailability] = useState(true);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+
   // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,80 +25,81 @@ const AddResources = () => {
 
     try {
       // Send a POST request to the API to create the resource
-      const headers={
+      const headers = {
         'Content-Type': "application/json",
-        'Authorization':`Bearer ${sessionStorage.getItem('_token')}` 
-      }
-      const response = await axios.post(`http://localhost:8080/api/n1/resource/create`,newResource,{headers})
-      console.log("API Response:", response);    
+        'Authorization': `Bearer ${sessionStorage.getItem('_token')}` 
+      };
+      const response = await axios.post(`http://localhost:8080/api/n1/resource/create`, newResource, { headers });
+      console.log("API Response:", response);
       toast.success("Resource created successfully");
-
-      // Display a success message
 
       // Clear form inputs after successful submission
       setName("");
       setQuantity("");
       setStatus("");
       setAvailability(true);
-      navigate("/resources")
+      navigate("/resources");
     } catch (error) {
       // Handle errors and display an error message
-      toast.error("Error creating resource");  
+      toast.error("Error creating resource");
       console.error("API Error:", error);
     }
   };
 
   return (
-    <div className="m-10">
-      <h2 className="text-3xl font-semibold mb-4 text-purple-700">
-        Add Resource
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Name</label>
-          <input
-            type="text"
-            className="w-full px-3 py-2 border rounded-md"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Quantity</label>
-          <input
-            type="number"
-            className="w-full px-3 py-2 border rounded-md"
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Status</label>
-          <input
-            type="text"
-            className="w-full px-3 py-2 border rounded-md"
-            value={status}
-            onChange={(e) => setStatus(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Availability</label>
-          <input
-            type="checkbox"
-            checked={availability}
-            onChange={(e) => setAvailability(e.target.checked)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded-full"
-        >
-          Create Resource
-        </button>
-      </form>
+    <div className="mx-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/3">
+      <h1 className="md:text-5xl sm:text-2xl overflow-hidden md:h-16 mt-10 text-center font-bold mb-4 text-indigo-600">Add Resource</h1>
+      <div className="bg-white rounded-lg shadow-lg p-6">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Name</label>
+            <input
+              type="text"
+              className="w-full px-3 py-2 border rounded-md"
+              value={name}
+              placeholder="Name of Resource"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Quantity</label>
+            <input
+              type="number"
+              className="w-full px-3 py-2 border rounded-md"
+              value={quantity}
+              placeholder="Quantity of resource"
+              onChange={(e) => setQuantity(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Status</label>
+            <input
+              type="text"
+              className="w-full px-3 py-2 border rounded-md"
+              value={status}
+              placeholder="Available, In-Use, Under Repair"
+              onChange={(e) => setStatus(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Availability</label>
+            <input
+              type="checkbox"
+              checked={availability}
+              onChange={(e) => setAvailability(e.target.checked)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-indigo-500 hover:bg-indigo-600 transition-all duration-200 text-white font-semibold py-3 px-6 rounded-full w-full"
+          >
+            Create Resource
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
-import apiConnector from "../services/apiConnector";
-import { resourceEndPoints } from "../services/api"; // Assuming you have resource endpoints defined
 import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 import ResoruceItem from "../components/ResoruceItem";
 import axios from "axios";
-
 
 const Resources = () => {
   const [resources, setResources] = useState([]);
@@ -39,30 +36,25 @@ const Resources = () => {
     navigate(`/resource-details/${resourceId}`);
   };
 
+  console.log(resources);
+
   return (
-    <div className="m-7">
-      <div className="my-3">
-        <Link to="/addResource">
-          <button className="px-4 py-2 bg-blue-500 rounded-full text-white hover:bg-blue-700">
+    <div className="w-11/12 mx-auto p-4">
+      <div className="mb-8">
+        <h2 className="md:text-5xl sm:text-2xl overflow-hidden md:h-16 text-center font-bold text-indigo-600">Resource Inventory</h2>
+      </div>
+      <div className="flex justify-center gap-x-16 items-center mb-4">
+        <Link className="w-6/12" to="/addResource">
+          <button className="px-4 w-full py-2 bg-indigo-500 rounded-full transition-all duration-200 text-white hover:bg-indigo-600">
             Add Resource
           </button>
         </Link>
       </div>
-      <div>
-        <h2 className="text-3xl font-semibold mb-4 text-purple-700">
-          Resource Inventory
-        </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {resources.map((resource) => (
+          <ResoruceItem key={resource._id} resource={resource} />
+        ))}
       </div>
-
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="flex flex-wrap mx-10">
-          {resources.map((resource) => (
-            <ResoruceItem key={resource._id} resource={resource} />
-          ))}
-        </div>
-      )}
     </div>
   );
 };
