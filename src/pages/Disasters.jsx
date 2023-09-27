9be2b1cec84f65fa52028801e97d2fac70c19c98
harshsindhu0408 <harshsindhu0402@gmail.com>
2023-09-27 21:21:18 +0530

    agency and disaster updated

--- src/pages/Disasters.jsx
+++ src/pages/Disasters.jsx
@@ -7,7 +7,12 @@ import DisasterComponent from "../components/DisasterComponent";
 
 const Disasters = () => {
   const [disasters, setDisasters] = useState([]);
+  const [filteredDisasters, setFilteredDisasters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [severityFilter, setSeverityFilter] = useState(""); // Severity filter
+  const [stateFilter, setStateFilter] = useState(""); // State filter
+  const [statusFilter, setStatusFilter] = useState(""); // Status filter
+  const [searchTerm, setSearchTerm] = useState(""); // Search term
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -30,42 +35,147 @@ const Disasters = () => {
     fetchData();
   }, []);
 
-  if (!disasters) {
-    return (
-      <div className="spinner w-full flex items-center justify-center"></div>
-    );
-  }
+  useEffect(() => {
+    // Filter disasters based on severity, state, and status
+    let filtered = disasters;
+
+    if (severityFilter) {
+      filtered = filtered.filter((disaster) =>
+        disaster.severity.toLowerCase() === severityFilter.toLowerCase()
+      );
+    }
+
+    if (stateFilter) {
+      filtered = filtered.filter((disaster) =>
+        disaster.contact.address.state.toLowerCase() === stateFilter.toLowerCase()
+      );
+    }
+
+    if (statusFilter) {
+      filtered = filtered.filter((disaster) =>
+        disaster.status.toLowerCase() === statusFilter.toLowerCase()
+      );
+    }
+
+    setFilteredDisasters(filtered);
+  }, [severityFilter, stateFilter, statusFilter, disasters]);
 
   const handleDetailsClick = (disasterId) => {
     // Navigate to the disaster details page with the disaster ID as a param
     navigate(`/disaster/${disasterId}`);
   };
 
+  const statesOfIndia = [
+    "Andaman and Nicobar Islands",
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chandigarh",
+    "Chhattisgarh",
+    "Dadra and Nagar Haveli and Daman and Diu",
+    "Delhi",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Lakshadweep",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Puducherry",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal",
+  ];
+
   return (
-    <div className="w-full bg-gray-100 flex  flex-col items-center justify-center">
+    <div className="w-full bg-gray-100 flex flex-col items-center justify-center">
       <div className="w-11/12 flex flex-col items-center justify-center">
-        {/* Add disaster button */}
-        <div className="w-8/12 mt-14 text-center">
-          <Link to="/addDisaster">
-            <button className="bg-indigo-500 sm:w-full md:w-4/12 md:text-2xl hover:bg-indigo-600 hover:scale-95 text-white shadow-sm rounded-full px-4 py-2 duration-300">
-              Add Disaster
-            </button>
-          </Link>
+        {/* Search bar */}
+        <div className="w-8/12 mt-6 mb-4">
+          <label className="block text-indigo-600 font-bold mb-2">Search:</label>
+          <input
+            type="text"
+            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Search any Disaster Info...."
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </div>
 
-        {/* List of disasters  */}
-        <div className="w-full mt-6">
-          <div className="w-full">
-            <h2 className="text-4xl ml-3 font-semibold mb-4 overflow-hidden text-indigo-600">
-              Disaster Events
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {disasters.map((disaster) => (
+        {/* Filters */}
+        <div className="w-8/12 gap-x-6 flex justify-between">
+          <div className="w-1/3">
+            <label className="block text-indigo-600 font-bold mb-2">Severity:</label>
+            <select
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => setSeverityFilter(e.target.value)}
+            >
+              <option value="">All</option>
+              <option value="high">High</option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+            </select>
+          </div>
+
+          <div className="w-1/3">
+            <label className="block text-indigo-600 font-bold mb-2">State:</label>
+            <select
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => setStateFilter(e.target.value)}
+            >
+              <option value="">All</option>
+              {statesOfIndia.map((state) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="w-1/3">
+            <label className="block text-indigo-600 font-bold mb-2">Status:</label>
+            <select
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => setStatusFilter(e.target.value)}
+            >
+              <option value="">All</option>
+              <option value="active">Active</option>
+              <option value="inactive">Inactive</option>
+            </select>
+          </div>
+        </div>
+
+        {/* List of disasters */}
+        <div className="w-full">
+          <h2 className="text-4xl text-center mt-6 font-bold mb-4 overflow-hidden text-indigo-600">
+            Disaster Events
+          </h2>
+          <div className="grid grid-cols-1 mb-20 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {filteredDisasters
+              .filter((disaster) =>
+                // Case-insensitive search
+                disaster.typeOfDisaster.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+              .map((disaster) => (
                 <div key={disaster._id}>
                   <DisasterComponent disaster={disaster} loading={loading} />
                 </div>
               ))}
-            </div>
           </div>
         </div>
       </div>
